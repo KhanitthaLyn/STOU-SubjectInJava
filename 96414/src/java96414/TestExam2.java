69e0be1ca55150005965374d8186384665124a0d
KhanitthaLyn <145004913+KhanitthaLyn@users.noreply.github.com>
2025-06-23 00:46:51 +0700

    TestExam2.java

--- 96414/src/java96414/TestExam2.java
+++ 96414/src/java96414/TestExam2.java
@@ -1,19 +1,12 @@
 package java96414;
 
-//6596007010 นางสาวขนิษฐา ตันตรงดี กลุ่ม 3
-
 public class TestExam2 {
 	public static void main(String[] args) {
-        // ประกาศและกำหนดค่าให้กับอาร์เรย์
         int[] numbers = {21, 44, 89, 54, 67, 78, 96, 37, 83, 59};
-
-        // แสดงข้อมูลในอาร์เรย์
         System.out.print("ข้อมูลในอาร์เรย์คือ : ");
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
         }
-
-        // ตรวจสอบและแสดงผลตัวเลขที่หารด้วย 11 ลงตัวพร้อม index ลำดับ
         System.out.print("\nตัวเลขที่หารด้วย 11 ลงตัว: ");
         boolean found = false;
         for (int i = 0; i < numbers.length; i++) {
@@ -22,8 +15,6 @@ public class TestExam2 {
                 found = true;
             }
         }
-
-        // ถ้าไม่มีตัวเลขที่หารด้วย 11 ลงตัว
         if (!found) {
             System.out.println("ไม่มีตัวเลขในอาร์เรย์ที่หารด้วย 11 ลงตัว");
         }
