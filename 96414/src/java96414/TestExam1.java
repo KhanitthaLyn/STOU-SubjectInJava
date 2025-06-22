1daa829ab96750f72be529302bebc1702467fbc9
KhanitthaLyn <145004913+KhanitthaLyn@users.noreply.github.com>
2025-06-23 00:47:23 +0700

    TestExam1.java

--- 96414/src/java96414/TestExam1.java
+++ 96414/src/java96414/TestExam1.java
@@ -1,18 +1,14 @@
 package java96414;
 import java.util.Scanner;
 
-//6596007010 นางสาวขนิษฐา ตันตรงดี กลุ่ม 3
-
 public class TestExam1 {
 
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // รับค่ารัศมี (Radius)
         System.out.print("กรอกรัศมีของกระบอก : ");
         double radius = scanner.nextDouble();
 
-        // รับค่าความสูง (Height)
         System.out.print("กรอกความสูงของกระบอก : ");
         double height = scanner.nextDouble();
         scanner.close();
