bb089a0409e165ada246d219634f1126fa605a6b
KhanitthaLyn <145004913+KhanitthaLyn@users.noreply.github.com>
2025-06-23 00:47:07 +0700

    TestExam3.java

--- 96414/src/java96414/TestExam3.java
+++ 96414/src/java96414/TestExam3.java
@@ -1,14 +1,12 @@
 package java96414;
 import java.util.Scanner;
 
-//6596007010 นางสาวขนิษฐา ตันตรงดี กลุ่ม 3
-
 public class TestExam3 {
 
 	public static void main(String[] args) {
         Square square = new Square();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("รอกความยาวด้านของสี่เหลี่ยม: ");
+        System.out.print("กรอกความยาวด้านของสี่เหลี่ยม: ");
         square.side = scanner.nextDouble();
         scanner.close();
 
